3f17d1cf009238e726e9bbc8b1d9a7365dc76c3c
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-10-04 12:03:04 +0100

    Se inicializa LOL API y se crea un array de campeones

--- src/js/Champion.js
+++ src/js/Champion.js
@@ -1,5 +1,9 @@
-export default class Champion() {
+export default class Champion {
     constructor(data) {
-        this.name 
+        this.name = data.name;
+        this.image = "https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/" + data.image.full;
+        this.title = data.title;
+        this.tags = data.tags;
+        this.blurb = data.blutb;
     }
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,17 @@
-const champions = [];
+import Champion from "./Champion.js";
 
+let champions = [];
+const loadingDataText = document.getElementById('loading-data-text');
+
+// Get champions from API
 async function getChampions() {
     const url = "https://ddragon.leagueoflegends.com/cdn/13.18.1/data/es_ES/champion.json";
-    let champions = await fetch(url)
-    .then(async (response) => {return (await response.json()).data})
-    
-    for (const key in champions) {
-        console.log(key);
+    const response = await fetch(url);
+    const data = (await response.json()).data;
+    for (let champion in data) {
+        champions.push(new Champion(data[champion]));
     }
+    loadingDataText.remove();
 }
 
 getChampions();
-console.log(champions);
