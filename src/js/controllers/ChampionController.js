bb20dd6652ed1d3aa056210bf1a7d8eb0079f9ef
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-10-07 22:08:18 +0100

    Se termina proyecto

--- src/js/controllers/ChampionController.js
+++ src/js/controllers/ChampionController.js
@@ -3,13 +3,23 @@ import ChampionView from "../views/ChampionViews.js";
 
 export default class ChampionController {
     constructor() {
-        this.model = new ChampionModel()
-        this.view = new ChampionView()
+        this.model = new ChampionModel();
+        this.view = new ChampionView();
+        this.init();
     }
 
     async init() {
         await this.model.fetchChampionsFromApi();
+        this.wait(5000)
         this.view.removeLoadingData();
         this.view.createGallery(this.model.getChampions());
     }
+
+    wait(ms){
+        var start = new Date().getTime();
+        var end = start;
+        while(end < start + ms) {
+          end = new Date().getTime();
+       }
+     }
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 import ChampionController from "./controllers/ChampionController.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    const controller = new ChampionController();
-    controller.init();
+    const controller = new ChampionController()
 });
